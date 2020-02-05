9c881cdd170e108f6fac94e1d5af0ae41348a391
ahj30 <ahj30@georgetown.edu>
2020-02-05 18:00:20 -0500

    Construct filepath reliably

--- reporter.py
+++ reporter.py
@@ -1,5 +1,5 @@
 
-
+import os
 import pandas 
 
 
@@ -22,13 +22,16 @@ print("READING GRADEBOOK CSV FILE...")
 
 
 # if CSV file in same dir as this python script:
-csv_filepath = "gradebook2.csv"
+# csv_filepath = "gradebook2.csv"
 
+# if CSV file in the data dir:
+
+csv_filepath = os.path.join(os.path.dirname(__file__), "data", "gradebook.csv")
+print("FILEPATH:", os.path.abspath(csv_filepath))
 
 grades = pandas.read_csv(csv_filepath)
 print("GRADES:", type(grades))
 #print(dir(grades))
-
 print(grades.tail())
 
 
